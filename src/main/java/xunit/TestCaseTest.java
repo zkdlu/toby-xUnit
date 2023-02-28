fcbb322d1fe991535df4fcd2010e2364978e9ac5
zkdlu <zkdlu159951@gmail.com>
2023-03-01 00:01:53 +0900

    junit 데모

---
+++ src/main/java/xunit/TestCaseTest.java
@@ -0,0 +1,15 @@
+package xunit;
+
+public class TestCaseTest extends TestCase {
+
+    public TestCaseTest(String name) {
+        super(name);
+    }
+
+    public void testRunning() {
+        WasRun wasRun = new WasRun("testMethod");
+        Assert.assertEquals(false, wasRun.wasRun);
+        wasRun.run();
+        Assert.assertEquals(true, wasRun.wasRun);
+    }
+}
--- src/main/java/xunit/XUnitTest.java
+++ src/main/java/xunit/XUnitTest.java
@@ -2,9 +2,6 @@ package xunit;
 
 public class XUnitTest {
     public static void main(String[] args) {
-        WasRun wasRun = new WasRun("testMethod");
-        System.out.println(wasRun.wasRun);
-        wasRun.run();
-        System.out.println(wasRun.wasRun);
+        new TestCaseTest("testRunning").run();
     }
 }
