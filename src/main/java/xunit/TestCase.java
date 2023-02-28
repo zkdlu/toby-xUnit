8a57cea796e6b62d7b0220c75b2b044629134b3a
zkdlu <zkdlu159951@gmail.com>
2023-02-28 23:57:39 +0900

    test 프로토타입

---
+++ src/main/java/xunit/TestCase.java
@@ -0,0 +1,21 @@
+package xunit;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class TestCase {
+    protected final String name;
+
+    public TestCase(String name) {
+        this.name = name;
+    }
+
+    public void run() {
+        try {
+            Method method = getClass().getMethod(name);
+            method.invoke(this);
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- src/main/java/xunit/WasRun.java
+++ src/main/java/xunit/WasRun.java
@@ -1,26 +1,13 @@
 package xunit;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-
-public class WasRun {
-    private final String name;
+public class WasRun extends TestCase {
     public boolean wasRun;
 
     public WasRun(String name) {
-        this.name = name;
+        super(name);
     }
 
     public void testMethod() {
         wasRun = true;
-    }
-
-    public void run() {
-        try {
-            Method method = getClass().getMethod(name);
-            method.invoke(this);
-        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
